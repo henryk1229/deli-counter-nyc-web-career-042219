262e3877c729aa9ba8b8469ce203acec972787e0
Henry Koehler <henryk1229@gmail.com>
2019-04-03 19:20:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  puts "Welcome, #{name}. You are number #{katz_deli.index+1} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 
