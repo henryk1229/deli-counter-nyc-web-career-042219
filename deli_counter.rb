2a5051dbc344fae967fcda1f17e377957c2b5cde
Henry Koehler <henryk1229@gmail.com>
2019-04-03 19:03:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,5 @@ def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
   else 
-    katz_deli.each_with_index do |name, number| 
-      puts " The line is currently: #{number+1}. #{name}"
-    end
-  end
-end
+    
       
