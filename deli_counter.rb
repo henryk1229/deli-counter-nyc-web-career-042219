80645857cdde9ccf018014ae6aabf5ca35d24e5f
Henry Koehler <henryk1229@gmail.com>
2019-04-03 19:07:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,16 @@
 katz_deli = []
 
 def line(katz_deli)
+  array = []
   if katz_deli.length == 0 
     puts "The line is currently empty."
   else 
+    katz_deli.each_with_index do |name, index|
+      array.push("#{index+1}. #{name}")
+    end
+    puts "The line is currenty #{array}"
+  end
+end
+
     
       
